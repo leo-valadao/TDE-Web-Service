c1f5b0d5dbbfa358bf99ad8ff577b515827d996f
Leonardo Valadão <leonardovo.iel@fieg.com.br>
2022-10-24 21:53:01 -0300

    6ª Atualização do TDE de Web Service.

--- src/main/java/com/leonardo/tde/domain/enumerable/EstadoPagamento.java
+++ src/main/java/com/leonardo/tde/domain/enumerable/EstadoPagamento.java
@@ -4,9 +4,6 @@ public enum EstadoPagamento {
 
     APROVADO("Aprovado"), EM_ANALISE("Em Análise"), REPROVADO("Reprovado");
 
-    private String tipo;
-
     private EstadoPagamento(String tipo){
-        this.tipo = tipo;
     }
 }
